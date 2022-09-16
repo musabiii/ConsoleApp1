e65410c542e0f940f29e6792c89a2e3a1eba10c2
Musabii Mambetov <musabi@live.com>
2022-09-17 00:31:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Company
+{
+    public string title = "Sun";
+}
+
+class Person
+    {
+        public string name;
+        public int age;
+        public Company company;
+
+        public void sayMe()
+        {
+            Console.WriteLine($"My name is {name}, and age is {age}, company {company.title}");
+        }
+
+        public Person(string name = "musa", int age = 21)
+        {
+            Console.WriteLine("this is a constructor");
+            this.name = name;
+            this.age = age;
+            company = new Company();
+        }
+        public void Deconstruct(out string nname, out int aage)
+            {
+                nname = name;
+                aage = age;
+            }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+User musa = new User {age=11,name="musa"};
+musa.sayHi();
+User alby = musa with { name="alby" };
+alby.sayHi();
+
+
+struct User
+{
+    public string name;
+    public int age;
+    public void sayHi()
+    {
+        Console.WriteLine($"name:{name},age:{age}");
+    }
+}
